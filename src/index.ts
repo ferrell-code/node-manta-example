3a97a30b7f57d4bc70e234a8553f0f242ca85e75
Charles Ferrell <charlie@manta.network>
2023-03-22 13:42:04 -0400

    add prettier and fmt
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { MantaPrivateWallet, Environment, Network } from 'manta.js/node';
-import BN from 'bn.js';
+import { MantaPrivateWallet, Environment, Network } from "manta.js/node";
+import BN from "bn.js";
 // @ts-ignore
-import { Keyring } from '@polkadot/keyring';
+import { Keyring } from "@polkadot/keyring";
 // @ts-ignore
-import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { cryptoWaitReady } from "@polkadot/util-crypto";
 
 // ES Module
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 // @ts-ignore
 global.fetch = fetch;
@@ -18,48 +18,64 @@ global.Request = fetch.Request;
 global.Response = fetch.Response;
 
 async function main() {
-	const privateWalletConfig = {
-		environment: Environment.Development,
-		network: Network.Dolphin,
-		loggingEnabled: true,
-	}
+  const privateWalletConfig = {
+    environment: Environment.Development,
+    network: Network.Dolphin,
+    loggingEnabled: true,
+  };
 
-	await cryptoWaitReady();
-	// Create a keyring instance
-	const keyring = new Keyring({ type: 'sr25519' });
-	const alice = keyring.addFromUri('//Alice');
+  await cryptoWaitReady();
+  // Create a keyring instance
+  const keyring = new Keyring({ type: "sr25519" });
+  const alice = keyring.addFromUri("//Alice");
 
-	const privateWallet = await MantaPrivateWallet.init(privateWalletConfig);
-	const privateAddress = await privateWallet.getZkAddress();
-    console.log("The private address is: ", privateAddress);
-	await privateWallet.initalWalletSync();
+  const privateWallet = await MantaPrivateWallet.init(privateWalletConfig);
+  const privateAddress = await privateWallet.getZkAddress();
+  console.log("The private address is: ", privateAddress);
+  await privateWallet.initalWalletSync();
 
-	const dolphinId = new BN(1);
-	const thousandDol = new BN("1000000000000000000000");
-	const toPrivateTx = await privateWallet.toPrivateBuild(dolphinId, thousandDol, alice, alice.address);
-	await toPrivateTx?.txs[0].signAndSend(alice, {});
+  const dolphinId = new BN(1);
+  const thousandDol = new BN("1000000000000000000000");
+  const toPrivateTx = await privateWallet.toPrivateBuild(
+    dolphinId,
+    thousandDol,
+    alice,
+    alice.address
+  );
+  await toPrivateTx?.txs[0].signAndSend(alice, {});
 
-	// wait for completion (better way than just sleeping?)
-	await new Promise(r => setTimeout(r, 10000));
-	await privateWallet.walletSync();
+  // wait for completion (better way than just sleeping?)
+  await new Promise((r) => setTimeout(r, 10000));
+  await privateWallet.walletSync();
 
-	const newPrivateBalance = await privateWallet.getPrivateBalance(dolphinId);
-	console.log("private balance of DOL: ", newPrivateBalance?.toString());
+  const newPrivateBalance = await privateWallet.getPrivateBalance(dolphinId);
+  console.log("private balance of DOL: ", newPrivateBalance?.toString());
 
-	const fiveHundredDol = thousandDol.div(new BN(2));
-	const toPublicTx = await privateWallet.toPublicBuild(dolphinId, fiveHundredDol, alice, alice.address);
-	await toPublicTx?.txs[0].signAndSend(alice, {});
+  const fiveHundredDol = thousandDol.div(new BN(2));
+  const toPublicTx = await privateWallet.toPublicBuild(
+    dolphinId,
+    fiveHundredDol,
+    alice,
+    alice.address
+  );
+  await toPublicTx?.txs[0].signAndSend(alice, {});
 
-	// wait for completion (better way than just sleeping?)
-	await new Promise(r => setTimeout(r, 10000));
-	await privateWallet.walletSync();
+  // wait for completion (better way than just sleeping?)
+  await new Promise((r) => setTimeout(r, 10000));
+  await privateWallet.walletSync();
 
-	const afterToPublicPrivateBalance = await privateWallet.getPrivateBalance(dolphinId);
-	console.log("private balance of DOL: ", afterToPublicPrivateBalance?.toString());
+  const afterToPublicPrivateBalance = await privateWallet.getPrivateBalance(
+    dolphinId
+  );
+  console.log(
+    "private balance of DOL: ",
+    afterToPublicPrivateBalance?.toString()
+  );
 
-	// disconnect api
-	privateWallet.api.disconnect();
+  // disconnect api
+  privateWallet.api.disconnect();
 }
 
-main().then().catch((e) => console.error(e));
-
+main()
+  .then()
+  .catch((e) => console.error(e));
